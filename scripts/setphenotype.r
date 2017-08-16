830294800f655f7eebd52d84acf2f7672e080c47
payamemami <payam.emami@medsci.uu.se>
2017-08-16 14:43:34 +0200

    bug fixes and adding more scripts

--- scripts/setphenotype.r
+++ scripts/setphenotype.r
@@ -14,7 +14,11 @@ phenoDataColumn<-"phenoData"
 
 previousEnv<-NA
 output<-NA
-inputCSV<-NA
+output<-NA
+appendTo<-NA
+appendToName<-"realNames"
+cleanFileName=T
+outputcsv<-NA
 for(arg in args)
 {
   argCase<-strsplit(x = arg,split = "=")[[1]][1]
@@ -23,13 +27,48 @@ for(arg in args)
   {
     previousEnv=as.character(value)
   }
-   if(argCase=="inputCSV")
+ if(argCase=="inputraw")
+{
+  input=as.character(value)
+  if(file.info(input)$isdir & !is.na(file.info(input)$isdir))
   {
-    inputCSV=as.character(value)
+    rawFiles<-list.files(input,full.names = T)
+    
+  }else
+  {
+   
+     rawFiles<-sapply(strsplit(x = input,split = "\\;|,| |\\||\\t"),function(x){x})
+    
   }
+}
+if(argCase=="galaxyNames")
+  {
+    galaxyNames=as.character(value)
+  }
+  if(argCase=="realNames")
+  {
+    realNames=as.character(value)
+  }
+  if(argCase=="cleanFileName")
+  {
+    cleanFileName=as.logical(value)
+  }
+  if(argCase=="appendTo")
+  {
+    appendTo=as.character(value)
+  }
+  if(argCase=="appendToName")
+  {
+    appendToName=as.character(value)
+  }
+
   if(argCase=="phenoDataColumn")
   {
     phenoDataColumn=as.character(value)
+  }
+  if(argCase=="outputcsv")
+  {
+    outputcsv=as.character(value)
   }
   if(argCase=="output")
   {
@@ -38,15 +77,76 @@ for(arg in args)
 }
 
 
-if(is.na(previousEnv) | is.na(output) | is.na(inputCSV)) stop("Both input and output need to be specified!\n")
+if(is.na(previousEnv) | is.na(output)) stop("Both input and output need to be specified!\n")
+
+
+
+
+galaxyNamesTMP<-gsub(pattern = " ",replacement = "",strsplit(x = galaxyNames,split = ",",fixed=T)[[1]],fixed=T)
+realNamesTMP<-gsub(pattern = " ",replacement = "",strsplit(x = realNames,split = ",",fixed=T)[[1]],fixed=T)
+
+galaxyNamesTMP[galaxyNamesTMP!=""]
+realNamesTMP[realNamesTMP!=""]
+
+
+if(cleanFileName)
+  realNamesTMP<-gsub(pattern = "Galaxy.*-\\[|\\].*",replacement = "",x = realNamesTMP)
+
+# can we have different length ?
+if(length(galaxyNamesTMP)!=length(realNamesTMP))
+{
+  stop("galaxy names and real file names have different length!")
+}
+
+# create a dataframe in which galaxynames have their realnames infront of them
+baseNames<-basename(galaxyNamesTMP)
+mappedNames<-data.frame(realNames=realNamesTMP,galaxyNames=baseNames,stringsAsFactors = F)
+mappedNames<-mappedNames[mappedNames[,1]!="",]
+if(!is.na(appendTo))
+{
+  tmpCSVFile<-read.csv(appendTo,stringsAsFactors = F)
+  sortedGalaxyNames<-mappedNames[match(tmpCSVFile[,appendToName],mappedNames[,"realNames"]),"galaxyNames"]
+  sortedGalaxyNamesID<-paste("step_",ncol(tmpCSVFile)+1,sep = "")
+  outCSVFile<-cbind(tmpCSVFile,tmp=sortedGalaxyNames)
+  colnames(outCSVFile)<-c(colnames(tmpCSVFile),sortedGalaxyNamesID)
+  mappedNames<-outCSVFile
+}else
+{
+  colnames(mappedNames)<-c(appendToName,"step_1")
+}
+
+# write the results
+write.csv(x = mappedNames,file = outputcsv,row.names = F)
+
+
+
+
+
+
 
 load(file = previousEnv)
 inputDataSet<-get(varNameForNextStep)
 
-fileNameMap<-read.csv(inputCSV)
+fileNameMap<-mappedNames
+
+fileNameMap$rawFiles<-NA
+# set raw file names
+rawFilesBaseName<-basename(rawFiles)
+rawFileDataFrame<-data.frame(rawFiles=rawFiles,rawFilesBaseName=rawFilesBaseName,stringsAsFactors = F)
+for(i in 1:nrow(rawFileDataFrame))
+{
+ rawFileIndex<-which(fileNameMap==rawFileDataFrame[i,"rawFilesBaseName"], arr.ind = TRUE)[1]   
+ fileNameMap[rawFileIndex,"rawFiles"]<-rawFileDataFrame[i,"rawFiles"]
+}
+
+
+if(any(is.na(fileNameMap[,"rawFiles"])))
+stop("Problem mapping the raw files")
+
 
 if(class(inputDataSet)=="xcmsSet")
 {
+  rawFilesSorted<-c()
   tmpDataFrame<-c()
   tmpPhenodata<-data.frame(inputDataSet@phenoData,stringsAsFactors = F)
   tmpPhenodata[,1]<-as.character(tmpPhenodata[,1])
@@ -55,13 +155,15 @@ if(class(inputDataSet)=="xcmsSet")
     
     PhenoIndex<-which(fileNameMap==rownames(tmpPhenodata)[i], arr.ind = TRUE)[1]   
     newPheno<-as.character(fileNameMap[PhenoIndex,phenoDataColumn])
-    tmpPhenodata[PhenoIndex,]<-newPheno
+    rawFilesSorted<-c(rawFilesSorted,as.character(fileNameMap[PhenoIndex,"rawFiles"]))
+    tmpPhenodata[i,]<-newPheno
     
     
   }
   tmpPhenodata[,1]<-factor(tmpPhenodata[,1])
   inputDataSet@phenoData<-tmpPhenodata
-  
+  inputDataSet@filepaths<-rawFilesSorted
+  rownames(inputDataSet@phenoData)<-basename(rawFilesSorted)
 }
 xcmsSetPhenoSet<-inputDataSet
 
